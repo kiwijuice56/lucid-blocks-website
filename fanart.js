913da7fde4c09e6b348eea5820621991c6df3abc
Eric Alfaro <ealfaro@mit.edu>
2025-10-17 15:31:28 -0400

    More fanart

--- fanart.js
+++ fanart.js
@@ -1,6 +1,7 @@
 const fanartList = [
     { src: "images/fanart/cheats_blamesman_discord.webp", credit: "cheats blamesman on discord" },
     { src: "images/fanart/ikouu_discord.webp", credit: "callmeiko on discord" },
+     { src:"images/fanart/ikouu_discord_pixel.png", credit: "callmeiko on discord" },
     { src: "images/fanart/d387_discord.png", credit: "d387 on discord" },
     { src: "images/fanart/kvivitulo_discord_bubblebear.webp", credit: "kvivitulo on discord" },
     { src: "images/fanart/hairic_bubblebear.png", credit: "hairic on discord" },
@@ -13,8 +14,18 @@ const fanartList = [
     { src: "images/fanart/heavywithcoping_discord.png", credit: "heavywithcoping on discord" },
     { src: "images/fanart/foxper_2.0_discord_bubblebears.jpg", credit: "foxper_04 on instagram" },
     { src: "images/fanart/foxper_2.0_discord.webp", credit: "foxper_04 on instagram" },
+    { src: "images/fanart/foxper_2.0_discord_3.jpg", credit: "foxper_04 on instagram" },
+    { src: "images/fanart/foxper_2.0_discord_4.jpg", credit: "foxper_04 on instagram" },
     { src: "images/fanart/para_discord.png", credit: "paranormilia on instagram" },
     { src: "images/fanart/evil_kasane_teto_discord.png", credit: "evil kasane teto on discord" },
+    { src: "images/fanart/faminemc_discord.png", credit: "faminemc on discord" },
+    { src: "images/fanart/gaby_noversam28_discord.png", credit: "gaby_noversam28 on discord" },
+    { src: "images/fanart/ld_anvil_discord.png", credit: "ld_anvil on discord" },
+    { src: "images/fanart/mr_karas__79880_discord_1.png", credit: "mr.karas__79880 on discord" },
+    { src: "images/fanart/mr_karas__79880_discord_2.png", credit: "mr.karas__79880 on discord" },
+    { src: "images/fanart/nonpartisanuno_discord.jpg", credit: "nonpartisanuno on discord" },
+    { src: "images/fanart/tofuuu_6334_discord.jpg", credit: "tofuuu6334 on discord" },
+    { src: "images/fanart/tofuuu_6334_discord_2.jpg", credit: "tofuuu6334 on discord" },
 ];
 
 function nextFanart() {
